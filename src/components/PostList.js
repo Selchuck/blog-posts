f16b3f65df056e5e458174e65c3b403036ce4889
Selchuk Karakus <selchuck.karakus@gmail.com>
2021-03-17 15:23:49 +0000

    create actions directory with index.js file, add first action creator, connect to PostList component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { fetchPosts } from '../actions';
+
 
 class PostList extends Component {
   render() {
@@ -8,4 +12,7 @@ class PostList extends Component {
   };
 };
 
-export default PostList;
+export default connect(
+  null,
+  { fetchPosts }
+)(PostList);
